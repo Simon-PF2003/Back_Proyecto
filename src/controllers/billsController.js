34df7a8b802b23087c1b47940a0e1e92aabb3817
Simon-PF2003 <ipcsimon.tel@gmail.com>
2025-02-16 00:15:02 -0300

    Caso de uso emision factura terminado. Corrección reporte de recaudación

--- src/controllers/billsController.js
+++ src/controllers/billsController.js
@@ -2,23 +2,26 @@ const Bill = require('../models/bill');
 const Order = require('../models/order');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+const { PDFDocument } = require('pdf-lib');
 
 exports.generateNewBill = async (req, res) => {
     try {
         console.log('generating bill');
         const { billData, pagado } = req.body;
         const { orderId } = billData;
-        console.log(orderId);
-        console.log(pagado);
+
         const order = await Order.findById(orderId).populate('userId');
         if (!order) {
             return res.status(400).json({ message: 'No se encontró el pedido' });
         }
+        
         const newBill = new Bill({ 
             orderId: order._id,
             userId: order.userId._id,
             items: order.items.map(item => ({
-                productId: item.productId,
+                productId: item._id,
                 desc: item.desc,
                 quantity: item.quantity,
                 price: 1.21 * item.price,
@@ -26,18 +29,73 @@ exports.generateNewBill = async (req, res) => {
             })),
             total: order.total * 1.21,           
         }); 
-        console.log('Factura a guardar', newBill);
+
         await newBill.save();
-        const token = jwt.sign({ _id: newBill._id }, 'secretKey');
+        
         const status = 'Facturado';
         await Order.findByIdAndUpdate(orderId, { status });
+
         if (pagado == false) {
             const userStatus = 'Moroso';
             await User.findByIdAndUpdate(order.userId._id, { status: userStatus });
         }
-        res.status(200).json({ token });
+
+        const pdfFile = await generateAndSavePDF(newBill._id);
+
+        res.setHeader('Content-Disposition', 'attachment; filename="factura.pdf"');
+        res.setHeader('Content-Type', 'application/pdf');
+        res.status(200).send(Buffer.from(pdfFile));
     } catch (error) {
         res.status(500).json({ message: 'Error generating bill', error: error.message });
+    }
+}
+
+const generateAndSavePDF = async (billId) => {
+    try {
+        const bill = await Bill.findById(billId).populate('userId');
+        if (!bill) {
+            throw new Error('No se encontró la factura');
+        }
+        
+        const templatePath = path.join(__dirname, '../templates/factura-rellenable.pdf');
+        const existingPdfBytes = fs.readFileSync(templatePath);
+
+        const pdfDoc = await PDFDocument.load(existingPdfBytes);
+        const form = pdfDoc.getForm();
+
+        const formattedDate = new Intl.DateTimeFormat('es-AR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit'
+        }).format(new Date(bill.createdAt));
+
+        form.getTextField('_id').setText(bill._id.toString());
+        form.getTextField('bill_date').setText(formattedDate);
+        form.getTextField('businessName').setText(bill.userId.businessName);
+        form.getTextField('address').setText(bill.userId.address);
+        form.getTextField('cuit').setText(bill.userId.cuit.toString());
+        form.getTextField('total').setText(bill.total.toFixed(2).toString());
+        form.getTextField('IVA').setText((bill.total * 0.21).toFixed(2).toString());
+        form.getTextField('subtotal').setText((bill.total / 1.21).toFixed(2).toString());
+
+        bill.items.forEach((item, index) => { 
+            if (index < 18) {
+                form.getTextField(`_id${index + 1}`).setText(item.productId);
+                form.getTextField(`cant${index + 1}`).setText(item.quantity.toString());
+                form.getTextField(`desc${index + 1}`).setText(item.desc);
+                form.getTextField(`iva${index + 1}`).setText('21%');
+                form.getTextField(`precio${index + 1}`).setText(item.price.toFixed(2).toString() + '$');
+                form.getTextField(`importe${index + 1}`).setText(item.totalPrice.toFixed(2).toString() + '$');
+            }
+        });
+
+        form.flatten();
+
+        const pdfBytes = await pdfDoc.save();
+        return pdfBytes;
+    
+    } catch (error) {
+        console.error('Error generating PDF:', error);
     }
 }
 
@@ -46,7 +104,7 @@ exports.getBills = async (req, res) => {
         console.log('Entramos a getBills');
         const { dateStart, dateEnd } = req.query;
 
-        // Validación de parámetros
+
         if (!dateStart || !dateEnd) {
             return res.status(400).json({ message: 'Debes ingresar un rango de fechas válido' });
         }
@@ -54,7 +112,6 @@ exports.getBills = async (req, res) => {
         let startDate = new Date(dateStart);
         let endDate = new Date(dateEnd);
 
-        // Verificar si las fechas son válidas
         if (isNaN(startDate) || isNaN(endDate)) {
             return res.status(400).json({ message: 'Formato de fecha inválido' });
         }
@@ -62,7 +119,6 @@ exports.getBills = async (req, res) => {
         // Ajustar endDate para incluir todo el día hasta las 23:59:59.999
         endDate.setHours(23, 59, 59, 999);
 
-        // Validaciones lógicas
         if (startDate > endDate) {
             return res.status(401).json({ message: 'La fecha de inicio debe ser menor o igual a la fecha de fin' });
         }
@@ -71,8 +127,6 @@ exports.getBills = async (req, res) => {
         if (startDate > now) {
             return res.status(402).json({ message: 'La fecha de inicio no puede ser mayor a la fecha actual' });
         }
-
-        // Obtener facturas dentro del rango
         const bills = await Bill.find({
             createdAt: { $gte: startDate, $lte: endDate }
         }).populate('userId');
@@ -82,7 +136,6 @@ exports.getBills = async (req, res) => {
         }
         console.log('Facturas encontradas:', bills.length);
 
-        // Calcular total recaudado
         const totalRecaudado = bills.reduce((acc, bill) => acc + (bill.total || 0), 0);
 
         res.status(200).json({ bills, totalRecaudado });
